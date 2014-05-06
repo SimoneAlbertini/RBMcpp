250fb382839fe52c3b7ed17bca0c85838dca26c2
SimoneAlbertini <sirion86@gmail.com>
2014-05-06 14:40:00 +0200

    added project

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: simone
+ *
+ * Created on May 6, 2014, 2:35 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
